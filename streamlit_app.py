6d80b283333450c8a889812fe29235a450289106
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-10-20 22:05:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,45 @@ import streamlit.components.v1 as components
 st.set_page_config(layout='wide')
 tab1,tab2 = st.tabs(['Current values', 'History'])
 with tab1:
+  components.html('''
+  <html>
+<head>
+<title>Data Collection Dashboard</title>
+<style>
+    button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+body
+    {
+        width:70%;
+        margin-left:auto;
+        margin-right:auto;
+    }
+</style>
+</head>
+<body>
+
+<table border=2 bordercolor="#0000FF">
+<tr><td colspan="2">
+<h1 align="center" color="#00FFFF">Data Collection Dashboard</h1>
+</td></tr>
+<tr><td>
+    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1843031/widgets/720669"></iframe>
+</td>
+<td><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1843031/widgets/720671"></iframe>
+</td></tr>
+<tr><td>
+    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1843031/widgets/720673"></iframe></td></tr>
+</html>
+  ''',height=600)
+with tab2:
   components.html(''' <html>
 <head>
 <title>Data Collection Dashboard</title>
@@ -26,10 +65,6 @@ body
 </style>
 </head>
 <body>
-    <a href="value.html">
-        <button>Current Values</button>
-      </a>
-      
 <table border=2 bordercolor="#0000FF">
 <tr><td colspan="2">
 <h1 align="center" color="#00FFFF">Data Collection Dashboard</h1>
@@ -43,4 +78,4 @@ body
     <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1843031/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Sulphur+%28MQ135%29&type=line&xaxis=Time&yaxis=PPM"></iframe></td></tr>
 
 </html>
-''',height=600,scrolling=True)
+''',height=600)
